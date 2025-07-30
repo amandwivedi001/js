9797a8730b9c4afc18383b03232ec430ee07783f
Aman Dwivedi <amandwivedi262@gmail.com>
2025-07-30 18:43:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc";
+
+// console.log(typeof score)
+
+let ValueInNumber = Number(score);
+
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0 
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => true
+// "" => false; 
+// "aman" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
